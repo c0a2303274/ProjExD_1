c799b3b9f57e1424c2875aaebbb6da9f9d789ecb
Okuda0921 <c0a2303274@edu.teu.ac.jp>
2024-06-11 16:49:25 +0900

    練習最後

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,33 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bgr_img = pg.transform.flip(bg_img, True, False)
     k_3 = pg.image.load("fig/3.png")
     k_3 = pg.transform.flip(k_3, True, False)
+    k_rect = k_3.get_rect()
+    k_rect.center = 300, 200
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
+        x = tmr % 3200
+        screen.blit(bg_img, [-(x), 0])
+        screen.blit(bgr_img, [1600 - (x), 0])
+        screen.blit(bg_img, [3200-(x), 0])
+        screen.blit(bgr_img, [4800 - (x), 0])
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            k_rect.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            k_rect.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:
+            k_rect.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            k_rect.move_ip(1, 0)
 
-        screen.blit(bg_img, [-(tmr%800), 0])
-        screen.blit(k_3, [300, 200])
+        screen.blit(k_3, k_rect)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
